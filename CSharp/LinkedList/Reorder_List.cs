a89dc6a0257f738b9ad28e33f880bf7de7ea01f9
icearl <icearl@qq.com>
2017-07-21 10:21:14 +0800

    //143 重排链表：链表倒序 快慢指针 合并链表

--- CSharp/LinkedList/Reorder_List.cs
+++ CSharp/LinkedList/Reorder_List.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//143 重排链表：链表倒序 快慢指针 合并链表
+//Reorder List
 namespace LinkedList {
     class Reorder_List {
-        // 前插法
-        public static void ReorderList(ListNode head) {
+        //链表长度分两半 + 前插法倒序 + 合并
+        public static void ReorderList1(ListNode head) {
             if (head == null || head.next == null) {
                 return;
             }
@@ -54,6 +55,49 @@ namespace LinkedList {
             // 如果总数是奇数，这一步是必要的。
             ppre.next.next = p2;
         }
+        // 快慢指针分两半 + 前插法倒序 + 合并
+        // 利用快慢两个指针将链表一分为二，针对第二个子链表求倒序，最后将两个子链表合并。
+        public static void ReorderList2(ListNode head) {
+            if (head == null || head.next == null) {
+                return;
+            }
+
+            // find the second half head
+            ListNode slow = head;
+            ListNode fast = head;
+            while (fast != null && fast.next != null) {
+                fast = fast.next.next;
+                slow = slow.next;
+            }
+
+            // reverse the second half
+            ListNode newHead = slow.next;
+            slow.next = null; // cut the first half
+            ListNode prev = null;
+            while (newHead != null) {
+                ListNode nextTemp = newHead.next;
+                newHead.next = prev;
+                prev = newHead;
+                newHead = nextTemp;
+            }
+
+            // combine two halves
+            ListNode l1 = head;
+            ListNode l2 = prev;
+            //ListNode ppre = null; 不用加这个，因为 l1 比 l2 多一个或两个元素
+            while (l1 != null && l2 != null) {
+                ListNode l1Next = l1.next;
+                ListNode l2Next = l2.next;
+
+                l1.next = l2;
+                l2.next = l1Next;
+
+                //ppre = l1;
+
+                l1 = l1Next;
+                l2 = l2Next;
+            }
+        }
 
         public void main() {
             ListNode node1 = new ListNode(1);
@@ -63,15 +107,15 @@ namespace LinkedList {
             ListNode node5 = new ListNode(5);
             ListNode node6 = new ListNode(6);
             ListNode node7 = new ListNode(7);
-            ListNode node8 = new ListNode(8);
+            //ListNode node8 = new ListNode(8);
             node1.next = node2;
             node2.next = node3;
             node3.next = node4;
             node4.next = node5;
             node5.next = node6;
             node6.next = node7;
-            node7.next = node8;
-            ReorderList(node1);
+            //node7.next = node8;
+            ReorderList2(node1);
             ListNode p = node1;
             while (p != null) {
                 Console.WriteLine(p.val);
